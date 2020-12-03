0008c797a64d3f4f7955e7836db2a32d57fdf782
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 18:56:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+var a
+var b
+var n
+
+function add(a,b) {
+return a+=b
+}
+
+function subtract(a,b) {
+return a-=b
+}
+
+function multiply(a,b) {
+return a*=b
+}
+
+function divide(a,b) {
+return a/+b
+}
+
+function increment(a){
+  return a++
+}
+
+function decrement(n){
+return  a--
+}
+
+function makeInt(a){
+  return parseInt(a, 10)
+}
+
+function preservedecimal(a){
+  return parsefloat(a)
+  return parseInt(sldkjflksjf, 10)
+}
